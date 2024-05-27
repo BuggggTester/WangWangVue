110d5d4ee476793fa9afd00955f0a5d40db10582
tpzzz123 <22371455@buaa.edu.cn>
2024-05-27 22:05:23 +0800

    修改了登陆界面，设置了其他cookie

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,76 @@
 <template>
   <div class="login-page">
-    <div class="background-image"></div>
-    <div class="login-container">
-      <span class="login">登录</span>
-    <div class="component">
-      <el-input v-model="account" placeholder="请输入账号" style="width: 70%; height: 40px;" :prefix-icon="User" clearable="true"></el-input>
-    </div>
-      <div class="component">
-        <el-input v-model="password" placeholder="请输入密码" style="width: 70%; height: 40px" :prefix-icon="User" type="password" show-password="true"></el-input>
-      </div>
-      <el-row>
-        <el-col :offset="4">
-        <el-button type="primary"  class="login-container-button"  @click="login">登录</el-button>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="2" :offset="4">
-        <el-checkbox v-model="remember" label="记住账号" size="large" />
-        </el-col>
-        <el-col :span="8" :offset="5">
-        <span style="font-size: 13px">没有账号？点此<span style="color:#25a4bb; cursor: pointer " @click="router.push('/register')">注册</span></span>
-        </el-col>
-      </el-row>
-    </div>
+<!--    <div class="background-image"></div>-->
+<!--    <div class="login-container">-->
+<!--      <span class="login">登录</span>-->
+<!--    <div class="component">-->
+<!--      <el-input v-model="account" :rules="loginRules" placeholder="请输入账号" style="width: 70%; height: 40px;" :prefix-icon="User" clearable="true"></el-input>-->
+<!--    </div>-->
+<!--    <div class="component">-->
+<!--        <el-input v-model="password" :rules="loginRules" placeholder="请输入密码" style="width: 70%; height: 40px" :prefix-icon="User" type="password" show-password="true"></el-input>-->
+<!--      </div>-->
+<!--      <el-row>-->
+<!--        <el-col :offset="4">-->
+<!--        <el-button type="primary"  class="login-container-button"  @click="login">登录</el-button>-->
+<!--        </el-col>-->
+<!--      </el-row>-->
+<!--      <el-row>-->
+<!--        <el-col :span="2" :offset="4">-->
+<!--        <el-checkbox v-model="remember" label="记住账号" size="large" />-->
+<!--        </el-col>-->
+<!--        <el-col :span="8" :offset="5">-->
+<!--        <span style="font-size: 13px">没有账号？点此<span style="color:#25a4bb; cursor: pointer " @click="router.push('/register')">注册</span></span>-->
+<!--        </el-col>-->
+<!--      </el-row>-->
+<!--    </div>-->
+    <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
+      <el-form-item prop="username">
+        <h1 class="title">登录</h1>
+        <el-input
+            v-model="loginForm.username"
+            type="text"
+            size="large"
+            auto-complete="off"
+            placeholder="账号"
+        >
+          <template #prefix><svg-icon icon="user" /></template>
+        </el-input>
+      </el-form-item>
+      <el-form-item style="width: 100%;">
+        <el-input
+            v-model="loginForm.password"
+            type="password"
+            size="large"
+            auto-complete="off"
+            placeholder="密码"
+            @keyup.enter="handleLogin"
+        >
+          <template #prefix><svg-icon icon="password" /></template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+      <el-checkbox v-model="loginForm.rememberMe">记住密码</el-checkbox>
+        <span class="toLogin" >忘记密码？</span>
+      </el-form-item>
+      <el-form-item style="width:100%;">
+        <el-button
+            size="large"
+            type="primary"
+            style="width:47%;"
+            @click.prevent="handleLogin"
+        >
+          <span>登 录</span>
+        </el-button>
+        <el-button
+            size="large"
+            type="default"
+            style="width:47%;"
+            @click.prevent="handleRegister"
+        >
+          <span>注 册</span>
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -34,64 +82,52 @@ import router from "@/router";
 import requestUtil from "@/util/request"
 import {ElMessage} from "element-plus";
 import cookieUtil from "@/util/cookie"
-const account = ref('');
-const password = ref('')
-let user = ref([])
-const remember = ref(false)
-const login = async() => {
-  console.log(remember.value)
-  if(account.value.length<=1) {
-    ElMessage({
-      message: "请输入昵称！",
-      type: "warning"
-    })
-    return false;
-  }
-  if(password.value.length <= 1){
-    ElMessage({
-      message: "请输入密码！",
-      type: "warning"
-    })
-    return false;
-  }
-  try {
-    user = await requestUtil.get('/user/select', {
-      "userName": account.value
-    })
-    console.log(user.data);
-  }catch (e) {
-    console.error(e);
-  }
 
-  let pwd = user.data.password;
-  console.log(user.data.user_name);
-  console.log(pwd);
-  if(pwd === password.value) {
-    cookieUtil.setCookie("userName", user.data.user_name, 1); // 1天过期
-    ElMessage({
-      message: "登录成功！",
-      type: "success"
-    })
-    await router.push('/main');
-    return true;
-  }
-  else if(user.data.user_name === "") {
-    ElMessage({
-      message: "登陆失败：用户名不存在！",
-      type: "warning"
-    });
-    return false;
-  }
-  else if(pwd !== password.value){
-    console.log(user.data);
+const loginRef = ref(null);
+const loginForm =ref({
+  username: "",
+  password: "",
+  rememberMe: false
+})
+const loginRules = ref({
+  account: [{ required: true, trigger: "blur", message: "请输入您的账号" }],
+  password: [{ required: true, trigger: "blur", message: "请输入您的密码" }],
 
-    ElMessage({
-      message: "登录失败：密码错误",
-      type: "warning"
-    });
-    return false;
-  }
+})
+const handleLogin = ()=>{
+  loginRef.value.validate(async(valid)=> {
+    if(valid) {
+      const res = await requestUtil.post('/user/login',{
+        "userName": loginForm.value.username,
+        "password": loginForm.value.password
+      });
+      console.log(res);
+      if(loginForm.value.rememberMe) {
+        cookieUtil.setCookie("userName", loginForm.value.username, 3);
+        cookieUtil.setCookie("userId", res.data.userId, 3);
+        cookieUtil.setCookie("password", res.data.password,3);
+        cookieUtil.setCookie("avatar", res.data.avatar,3);
+        //TODO: 后续添加其他cookie
+      }else {
+        cookieUtil.setCookie("userName", loginForm.value.password, 1);
+        cookieUtil.setCookie("userId", res.data.userId, 1);
+        cookieUtil.setCookie("password", res.data.password,1);
+        cookieUtil.setCookie("avatar", res.data.avatar,1);
+      }
+      if(res.data.msg === 'login success'){
+        ElMessage({
+          message: "登陆成功！",
+          type: "success"
+        })
+        await router.push('/main');
+      }
+    }
+  })
 }
+const handleRegister = () => {
+  router.push('/register');
+}
+
 
 onMounted(async ()=>{
   const res = cookieUtil.getCookie("userName")
@@ -106,7 +142,47 @@ onMounted(async ()=>{
 </script>
 
 <style>
-@import "@/assets/css/login.css";
-@import "@/assets/css/card-image.css";
+.login-form {
+  border-radius: 6px;
+  background: #ffffff;
+  width: 400px;
+  padding: 25px 25px 5px 25px;
 
+  .el-input {
+    height: 40px;
+
+
+
+    input {
+      display: inline-block;
+      height: 40px;
+    }
+  }
+  .input-icon {
+    height: 39px;
+    width: 14px;
+    margin-left: 0px;
+  }
+
+}
+.login-page{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url("../assets/images/login/login.png");
+  background-size: cover;
+}
+.title {
+  margin: 0px auto 30px auto;
+  text-align: center;
+  color: #707070;
+  font-size: larger;
+  font-weight: bold;
+}
+.toLogin {
+  color: #25a4bb;
+  cursor: pointer;
+  margin-left: 60%;
+}
 </style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,33 +1,57 @@
 <template>
   <el-upload
-      v-model:file-list="fileList"
-      list-type="picture-card"
+      class="upload-demo"
+      action="http://localhost:1145/file/upload"
+      :file-list="fileList"
+      :on-change="handleChange"
+      :http-request="customHttpRequest"
       multiple
-      :auto-upload="false"
-      :on-preview="handlePictureCardPreview"
-      :on-remove="handleRemove"
-  >
-    <el-icon><Plus /></el-icon>
+      :limit="3"
+      :on-exceed="handleExceed"
+      :auto-upload="false">
+    <el-button size="small" type="primary">点击上传</el-button>
+    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
   </el-upload>
-
-  <el-dialog v-model="dialogVisible">
-    <img w-full :src="dialogImageUrl" alt="Preview Image" />
-  </el-dialog>
-  <el-button @click="submit">提交</el-button>
+  <img :src="imageUrl">
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { Plus } from "@element-plus/icons-vue";
+import requestUtils, {getServerUrl} from "@/util/request";
 import { ElMessage } from "element-plus";
+import { ref } from "vue";
+    // const imageUrl = ref("http://localhost:1145/images/avatars/b2d139e3-27b7-47c0-9102-1e917814f310.png");
+    const imageUrl = getServerUrl()+"images/user.png";
+    console.log(imageUrl);
+    const fileList = ref([]);
 
-const submit = () => {
+    const handleChange = (file, fileList) => {
+      if (file.raw) {
+        const formData = new FormData();
+        formData.append('File', file.raw);
 
-};
+        requestUtils.fileUpload('file/upload', formData)
+            .then(response => {
+              imageUrl.value = response.data.imageUrl; // 更新imageUrl为上传成功后的图片URL
+              ElMessage({
+                message: "上传成功！",
+                type: "success"
+              });
+            })
+            .catch(error => {
+              ElMessage({
+                message: "上传失败！",
+                type: "warning"
+              });
+            });
 
-const fileList = ref();
+    };
 
-const dialogImageUrl = ref("");
-const dialogVisible = ref(false);
+    const customHttpRequest = (file) => {
+      // 自定义上传逻辑，如果需要可以在这里进行处理
+    };
 
+    const handleExceed = (files, fileList) => {
+      ElMessage.warning('最多只能上传 3 张图片');
+    };
+}
 </script>
